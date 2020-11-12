652223445607a898502b3ee71cb72ae094736961
B1zzy1 <wil06we.com@gmail.com>
2020-11-12 13:43:38 -0300

    Updates

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,15 @@
+provider "aws" {
+  profile = "${var.aws_profile}"
+  region = "${var.aws_region}"
+}
+
 resource "aws_cloudwatch_log_group" "lambda-log-group" {
   name = "UpdateCloudflareIps"
-  count = "${var.enabled == "true" ? 1 : 0}"
+
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+
   name = "lambda-cloudflare-role"
   assume_role_policy = <<EOF
 {
@@ -25,7 +30,7 @@ EOF
 
 resource "aws_iam_policy" "policy" {
   name = "lambda-cloudflare-policy"
-  count = "${var.enabled == "true" ? 1 : 0}"
+
   description = "Allows cloudflare ip updating lambda to change security groups"
   policy = <<EOF
 {
@@ -61,20 +66,20 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "policy" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+
   role = "${aws_iam_role.iam_for_lambda.id}"
   policy_arn = "${aws_iam_policy.policy.arn}"
 }
 
 data "archive_file" "lambda_zip" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+
   type = "zip"
-  source_file = "${path.module}/cloudflare-security-group.py"
+  source_dir = "${path.module}/script/"
   output_path = "${path.module}/lambda.zip"
 }
 
 resource "aws_lambda_function" "update-ips" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+
   function_name = "UpdateCloudflareIps"
   filename = "${path.module}/lambda.zip"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
@@ -83,7 +88,7 @@ resource "aws_lambda_function" "update-ips" {
   runtime = "python3.6"
   timeout = 60
   environment {
-    variables {
+    variables = {
       SECURITY_GROUP_ID = "${var.security_group_id}"
     }
   }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
+variable "aws_region" {
+  description = "The AWS region"
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  default = "pagarme-security"
+}
+
 variable "security_group_id" {
   description = "An existing security group to populate with cloudflare ips as ingress rules."
+  default = "sg-08246d2d35dc83fc7"
 }
 variable "enabled" {
   description = "Whether to do anything at all, useful if cloudflare is not needed on all environments. Accepts the string 'true' or 'false'."
